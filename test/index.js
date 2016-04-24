e84d64e418a606c3e9db4d6f10be6a0bfed0f6c6
59naga <i59naga@icloud.com>
2016-04-24 16:19:27 +0900

    fix: use `shell-quote@1.6.0` refs substack/shell-quote#22

--- test/index.js
+++ test/index.js
@@ -19,6 +19,14 @@ test('should be able to run the bin', async (t) => {
   t.true(code === 0);
 });
 
+// @see https://github.com/substack/node-shell-quote/pull/22
+test('should be able to run the bin with comment', async (t) => {
+  const child = npmRunScript('eslint foo#comment', { stdio: 'ignore' });
+  const code = await result(child);
+
+  t.true(code === 0);
+});
+
 test('shell command should spawn in as much as possible', async (t) => {
   const child = npmRunScript('eslint && exit 59', { stdio: 'ignore' });
   const code = await result(child);
